0596865c7131856a31a79703baac3eabe009ab2f
Lauren McCarthy <laurmccarthy@gmail.com>
2016-08-17 12:59:05 -0600

    ok

--- geo/sketch.js
+++ geo/sketch.js
@@ -1,6 +1,11 @@
 var area1;
-var lat = '',
-  lon = '';
+var lat = '';
+var lon = '';
+var font;
+
+function preload() {
+  font = loadFont('Stellar-Medium.otf');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,35 +16,39 @@ function setup() {
   }
   textAlign(CENTER);
   fill(255);
+  textFont(font);
 }
 
 function draw() {
   if (area1) {
     background(0, 255, 0);
-    textSize(windowWidth * 0.1);
+    textSize(windowWidth * 0.3);
     if (area1.insideFence) {
       text("IN", width / 2, height / 2);
     } else {
       text("OUT", width / 2, height / 2);
     }
   } else {
-    background(255, 0, 0);
+    background(255, 100, 0);
   }
-  textSize(windowWidth*0.05);
-  text("lat: " + lat + "     lon: " + lon, width / 2, height * 0.2);
+  textSize(windowWidth*0.15);
+  text("LAT", width / 2, height * 0.32);
+  text(lat, width / 2, height * 0.42);
+  text("LON", width / 2, height * 0.57);
+  text(lon, width / 2, height * 0.67);
 }
 
 function positionUpdated(position) {
-  lat = nfc(position.latitude, 3);
-  lon = nfc(position.longitude, 3);
+  lat = nfc(position.latitude, 8);
+  lon = nfc(position.longitude, 8);
   print("lat: " + lat + " lon: " + lon);
 }
 
-function touchStarted() {
-  if (!area1) {
-    area1 = new geoFenceCircle(39.214, -106.937, 0.01);
-  } else {
-    area1.clear();
-    area1 = null;
-  }
-}
+// function touchStarted() {
+//   if (!area1) {
+//     area1 = new geoFenceCircle(39.214, -106.937, 0.01);
+//   } else {
+//     area1.clear();
+//     area1 = null;
+//   }
+// }
