bdcf6d42ee195fc8fbd6e65bd89c3e886d0d6f4c
Lauren McCarthy <laurmccarthy@gmail.com>
2016-08-18 09:13:30 -0600

    loc

---
+++ 07_location/testLocation/sketch.js
@@ -0,0 +1,31 @@
+var numLocations = 5; // set your total number of locations here
+var curLocation = 1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(127);
+
+  if (curLocation == 1) {
+    // draw view for location 1
+    background(255, 0, 0);
+  } else if (curLocation == 2) {
+    // draw view for location 2
+    background(255, 255, 0);
+  } else if (curLocation == 3) {
+    // draw view for location 3
+    background(255, 0, 255);
+  }
+
+  // this draws the testing buttons on top of everything
+  drawButtons();
+}
+
+function mousePressed() {
+  // this detects a click on the testing buttons
+  detectButtonPress();
+}
+
+
